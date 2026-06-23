5a34ec2b1176d5abd64ae1fd044982aa25401d89
isadoramendes02 <raynara.oliveira10auno.ce.gov.br>
2026-06-23 09:46:37 -0300

    arrumando os arquivos do crud

--- usuarios/delete.php
+++ usuarios/delete.php
@@ -1,12 +1,4 @@
 <?php
-
-session_start();
-
-if (!isset($_SESSION['usuario_logado'])) {
-    header("Location: ../login.php");
-    exit;
-}
-
 require_once "../conexao.php";
 
 $id = (int) $_GET['id'];
